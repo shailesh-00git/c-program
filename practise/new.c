c77660a46836826f57cace5ffa003c88d0d6de3a
shailesh-00git <sy468698@gmail.com>
2025-05-19 15:57:37 +0545

    no thing added

--- practise/new.c
+++ practise/new.c
@@ -1,6 +1,25 @@
+// to find the sum of the series: 1 + x - x^2/2 + x^3/3......to n terms
 #include <stdio.h>
+#include <math.h>
 int main()
 {
-    printf("hello world");
+    int n, i, x,sign = 1;
+    float sum = 1;
+    printf("Enter the value of x & number of terms: ");
+    scanf("%d%d", &x, &n);
+    for (i = 1; i <= n - 1; i++)
+    {
+        if (sign == 1)
+        {
+            sum = sum + (pow(x, i)) / i;
+            sign = 0;
+        }
+        else
+        {
+            sum = sum - (pow(x, i)) / i;
+            sign = 1;
+        }
+    }
+    printf("%f",sum);
     return 0;
 }
---
+++ practise/size.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    printf("The size of int = %u bytes\n",sizeof(int));
+    printf("The size of float = %u bytes\n",sizeof(float));
+    printf("The size of char = %u bytes\n",sizeof(char));
+    printf("The size of double = %u bytes\n",sizeof(double));
+    return 0;
+}
