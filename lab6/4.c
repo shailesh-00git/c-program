bf0b6d709ef577370b41813c5822f3270e3617de
shailesh-00git <sy468698@gmail.com>
2025-05-27 23:56:54 +0545

    fix in lab6

--- lab6/4.c
+++ lab6/4.c
@@ -1,35 +1,34 @@
-// To find the sum pf the series: 1 - x/1! + X^2/2!-x^3/3! + ..... to n terms
+// To find the sum of the series: 1 - x/1! + x^2/2! - x^3/3! + ... up to n terms
 #include <stdio.h>
 #include <math.h>
 int main()
 {
-    int i, n, x, op = 0, fact;
-    float  sum = 1.0;
+    int i, n, x, fact, sign = 0;
+    float sum = 1.0;
 
     printf("Enter the number of terms and value of x: ");
     scanf("%d%d", &n, &x);
-    for (i = 1; i <= n - 1; i++)
+
+    for (i = 1; i < n-1; i++)
     {
-        for (i = 1; i < n; i++)
+        fact = 1;
+        for (int j = 1; j <= i; j++)
         {
-            // Calculate factorial of i
-            fact = 1;
-            for (int j = 1; j <= i; j++)
-            {
-                fact *= j;
-            }
-            if (op == 0)
-            {
-                sum = sum - (pow(x, i) / fact);
-                op = 1;
-            }
-            else if (op == 1)
-            {
-                sum = sum + (pow(x, i) / fact);
-                op = 0;
-            }
+            fact *= j;
         }
+        if (sign == 0)
+        {
+            sum -= (pow(x, i) / fact);
+            sign = 1;
+        }
+        else{
+             sum += (pow(x, i) / fact);
+             sign = 0;
+        }
+
+        
     }
-    printf("The summ of series up to %d terms is : %f", n, sum);
+
+    printf("The sum of series up to %d terms is : %f\n", n, sum);
     return 0;
 }
