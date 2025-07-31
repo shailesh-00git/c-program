45104b6bcfa753394ec919f8ea5d4425b70207d4
shailesh-00git <sy468698@gmail.com>
2025-07-31 15:37:29 +0545

    Modified

--- string/11.c
+++ string/11.c
@@ -1,16 +1,32 @@
-//to find the numbe of vowels in  given string
+// to sort the strings into ascending order
 #include <stdio.h>
 #include <string.h>
+
 int main()
 {
-    int count = 0, i;
-    char str[20];
-    printf("Enter a string: ");
-    scanf("%s", str);
-    for (i = 0; str[i] != '\0'; i++)
+    char temp[10];
+    int i, j;
+    char name[3][10] = {"ram", "sita", "hari"};
+
+    // Sort strings using bubble sort
+    for (i = 0; i < 3; i++)
     {
-        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
-            count++;
+        for (j = i + 1; j < 3; j++)  // Changed 4 to 3
+        {
+            if (strcmp(name[i], name[j]) > 0)  // Compare strings, not indices
+            {
+                strcpy(temp, name[i]);
+                strcpy(name[i], name[j]);
+                strcpy(name[j], temp);
+            }
+        }
     }
-    printf("There are %d vowels", count);
+
+    // Print sorted strings
+    printf("Strings in ascending order:\n");
+    for (i = 0; i < 3; i++)
+    {
+        printf("%s\n", name[i]);
+    }
+    return 0;
 }
